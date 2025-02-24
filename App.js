84070514ed02e0d726a68c7b75d55a08eaa3b7e5
rizwana573 <mehar.rizwana573@gmail.com>
2025-02-24 21:21:07 +0530

    intro

---
+++ App.js
@@ -0,0 +1,11 @@
+const parent = React.createElement("div", {id:"parent"},[
+    React.createElement("div", {id:"child1"},[
+    React.createElement("span",{id:"grandChild1"}, "Hello from child1!"),
+]),
+    React.createElement("div", {id:"child2"},[
+    React.createElement("span",{id:"grandChild2"}, "Hello from child2!")
+]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
