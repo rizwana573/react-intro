e68055b86327affe71967f5510a9bdf8016caebe
rizwana573 <mehar.rizwana573@gmail.com>
2025-05-02 00:33:12 +0530

    other

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", {id:"parent"},[
     React.createElement("div", {id:"child1"},[
     React.createElement("span",{id:"grandChild1"}, "Hello from child1!"),
 ]),
     React.createElement("div", {id:"child2"},[
-    React.createElement("span",{id:"grandChild2"}, "Hello from child2!")
+    React.createElement("span",{id:"grandChild2"}, "Hello from child2ss!")
 ]),
 ]);
 
---
+++ README.md
@@ -0,0 +1,9 @@
+#parcel
+supports hmr : hot module replacement
+dev build
+local server
+caching- faster builds
+file watching algorithms - written in c++
+image optimization
+minification,compression and bundling
+
